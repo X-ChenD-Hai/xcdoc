45afef129117659a888f4897d4f855e24fb849eb
X_ChenD_Hai <illuminatestar@foxmail.com>
2025-07-26 08:19:23 +0800

    feat(CppParser): 添加对GCC和Clang编译数据库的支持
    为不同编译器（GCC和Clang）添加了对编译数据库的支持，分别在代码中添加了针对GCC和Clang编译数据库的处理逻辑。
    
    - 添加了对GCC编译数据库的支持：
      - 使用`GCC_COMPILE_DATABASE`宏来判断是否启用GCC编译数据库的处理。
      - 创建了一个`CompileSystem`对象`gcc_system`，并用`utils::read_file(GCC_COMPILE_DATABASE)`初始化。
      - 遍历`gcc_system`中的每个编译单元`unit`，并输出其关联的词法分析器`lexer`的名称。
    
    - 添加了对Clang编译数据库的支持：
      - 使用`CLANG_COMPILE_DATABASE`宏来判断是否启用Clang编译数据库的处理。
      - 创建了一个`CompileSystem`对象`clang_system`，并用`utils::read_file(CLANG_COMPILE_DATABASE)`初始化。
      - 遍历`clang_system`中的每个编译单元`unit`，并输出其关联的词法分析器`lexer`的名称。
    
    - 修正了原有Clang编译数据库的处理逻辑：
      - 将原有的`CompileSystem system`改为`CompileSystem clang_cl_system`，以避免变量名冲突。
      - 保留了对Clang编译数据库的处理逻辑，并确保其功能正确无误。

--- tests/CppParser/CppParser.cc
+++ tests/CppParser/CppParser.cc
@@ -5,8 +5,35 @@
 TEST(CppParser, CppParser) {
 #ifdef CLANG_CL_COMPILE_DATABASE
     try {
-        CompileSystem system(utils::read_file(CLANG_CL_COMPILE_DATABASE));
-        for (auto& unit : system.units()) {
+        CompileSystem clang_cl_system(
+            utils::read_file(CLANG_CL_COMPILE_DATABASE));
+        for (auto& unit : clang_cl_system.units()) {
+            for (auto& it : unit->attached_lexers()) {
+                OUT SV(if, it.first) ENDL;
+            }
+            // OUT VV(unit->pre_lexer()->source()) ENDL;
+        }
+    } catch (const std::exception& e) {
+        FAIL() << e.what();
+    }
+#endif
+#ifdef GCC_COMPILE_DATABASE
+    try {
+        CompileSystem gcc_system(utils::read_file(GCC_COMPILE_DATABASE));
+        for (auto& unit : gcc_system.units()) {
+            for (auto& it : unit->attached_lexers()) {
+                OUT SV(if, it.first) ENDL;
+            }
+            // OUT VV(unit->pre_lexer()->source()) ENDL;
+        }
+    } catch (const std::exception& e) {
+        FAIL() << e.what();
+    }
+#endif
+#ifdef CLANG_COMPILE_DATABASE
+    try {
+        CompileSystem clang_system(utils::read_file(CLANG_COMPILE_DATABASE));
+        for (auto& unit : clang_system.units()) {
             for (auto& it : unit->attached_lexers()) {
                 OUT SV(if, it.first) ENDL;
             }
