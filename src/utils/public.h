f8f1f8368ccbd844246c064faabc7f21179cf553
X_ChenD_Hai <illuminatestar@foxmail.com>
2025-07-28 17:22:41 +0800

    feat(CppParser): 添加SourceLexer以支持源代码分析
    - 在`CMakeLists.txt`中添加了`SourceLexer.re`以生成C++源代码解析器文件。
    - 新建`SourceLexer.h`，定义了`CppSymbol`类用于表示C++符号，并定义了`SourceLexer`类用于解析源代码。
    - 新建`SourceLexer.re`，实现了`SourceLexer`构造函数的具体逻辑。
    - 新建`main.cc`，作为程序入口文件，目前仅包含一个待实现的`main`函数。
    - 在`utils/public.h`中添加了必要模板和宏定义，包括`unique_list_t`, `shared_list_t`, `weak_list_t`类型定义以及`TODO`宏，用于调试和标记待处理的代码。

--- src/utils/public.h
+++ src/utils/public.h
@@ -1,4 +1,5 @@
 #pragma once
+#include <cassert>
 #include <iostream>  // IWYU pragma: keep
 #include <string>
 
@@ -24,4 +25,25 @@
 namespace utils {
 std::string read_file(const std::string &filename);
 std::string unquote(std::string_view raw);
+using string_list_t = std::vector<std::string>;
+template <typename T>
+using unique_list_t = std::vector<std::unique_ptr<T>>;
+template <typename T>
+using shared_list_t = std::vector<std::shared_ptr<T>>;
+template <typename T>
+using weak_list_t = std::vector<std::weak_ptr<T>>;
+
+#ifdef __xcdoc_debug__
+#define TODO                                                 \
+    do {                                                     \
+        ERR << __FILE__ << ":" << __LINE__ << " TODO " ENDL; \
+        assert(false && "Called TODO");                      \
+    } while (1)
+#else
+#define TODO                            \
+    do {                                \
+        assert(false && "Called TODO"); \
+    } while (1)
+#endif
+
 }  // namespace utils
